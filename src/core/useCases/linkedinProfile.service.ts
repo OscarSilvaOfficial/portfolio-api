4de888bde34c2f52adb9f99a20fd95fe96b4b832
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-02-11 00:30:25 -0300

    Aplicação de DTO

--- src/core/useCases/linkedinProfile.service.ts
+++ src/core/useCases/linkedinProfile.service.ts
@@ -1,6 +1,7 @@
 import { LinkedinPort } from 'src/ports/linkedin.port';
 import { LinkedinRepositoryPort } from '../../ports/linkedin.repository.port';
 import { IProfile } from '../domain/interfaces/profile.interface';
+import { Profile } from '../domain/profile';
 
 class linkedinProfileService {
   repository: LinkedinRepositoryPort;
@@ -14,8 +15,25 @@ class linkedinProfileService {
     this.linkedinAdapter = linkedinAdapter;
   }
 
+  linkedinProfileDTO(responseData: any): Profile {
+    const data = { ...responseData[0]._doc };
+    const profile = new Profile(
+      data.profile_pic_url,
+      data.full_name,
+      data.occupation,
+      data.headline,
+      data.summary,
+      data.experiences,
+      data.educations,
+      data.certifications,
+      data.updatedAt ? new Date(data.updatedAt) : new Date(),
+    );
+    return profile;
+  }
+
   async getLikedinProfile(): Promise<IProfile> {
-    return await this.repository.getLikedinProfile();
+    const profileData = await this.repository.getLikedinProfile();
+    return this.linkedinProfileDTO(profileData);
   }
 }
 

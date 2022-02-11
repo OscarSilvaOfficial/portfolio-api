9757abb514a0331f5484a311ef2b9a16c14ce032
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-02-11 00:31:10 -0300

    Ajuste na separação de responsabilidade

--- src/infra/external/likedin.adapter.ts
+++ src/infra/external/likedin.adapter.ts
@@ -1,10 +1,7 @@
 import { LinkedinPort } from '../../ports/linkedin.port';
 import { Injectable } from '@nestjs/common';
-import { Profile } from 'src/core/domain/profile';
 import { IProfile } from 'src/core/domain/interfaces/profile.interface';
 import { RequestPort } from 'src/ports/request.port';
-import { AxiosAdapter } from './axios.adapter';
-import info from '../../../linkedin.json';
 
 @Injectable()
 class LinkedinAdapter implements LinkedinPort {
@@ -13,32 +10,6 @@ class LinkedinAdapter implements LinkedinPort {
 
   constructor(requestAdapter: RequestPort) {
     this.request = requestAdapter;
-  }
-
-  private replaceValues(profile: any): IProfile {
-    profile.experiences.map((experience: any) => {
-      if (
-        !(experience.starts_at instanceof Date) &&
-        !(experience.ends_at instanceof Date)
-      ) {
-        experience.starts_at = new Date(
-          experience.starts_at.year,
-          experience.starts_at.month,
-          experience.starts_at.day,
-        );
-
-        if (experience.ends_at) {
-          experience.ends_at = new Date(
-            experience.ends_at.year,
-            experience.ends_at.month,
-            experience.ends_at.day,
-          );
-        } else {
-          experience.ends_at = null;
-        }
-      }
-    });
-    return profile;
   }
 
   private getDataOnLinkedIn(): Promise<IProfile> {
@@ -52,30 +23,8 @@ class LinkedinAdapter implements LinkedinPort {
     return this.request.get(requestUrl);
   }
 
-  private mountProfile(payload: any): Profile {
-    return new Profile(
-      payload.profile_pic_url,
-      payload.first_name + ' ' + payload.last_name,
-      payload.occupation,
-      payload.headline,
-      payload.summary,
-      payload.experiences,
-      payload.educations,
-      payload.certifications,
-    );
-  }
-
-  async defineQuery(linkedinQuery: boolean): Promise<any> {
-    if (linkedinQuery) {
-      const response = await this.getDataOnLinkedIn();
-      return response;
-    }
-    return { data: this.replaceValues(info) };
-  }
-
-  async getLikedinProfile(linkedinQuery: boolean): Promise<Profile> {
-    const response = await this.defineQuery(linkedinQuery);
-    const profile = this.mountProfile(response.data);
+  async getLikedinProfile(): Promise<any> {
+    const profile = await this.getDataOnLinkedIn();
     return new Promise((resolve) => resolve(profile));
   }
 }

5bcf163327538b2472be232fdffaff7a28cf3b80
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-02-13 03:50:03 -0300

    Ajuste na porta

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configSwegger(app);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();

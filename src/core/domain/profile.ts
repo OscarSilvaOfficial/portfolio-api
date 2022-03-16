cdf9fcce56694d4a0b5ff9990e8d4ce3e46e611e
OscarSilvaOfficial <OscarSilvaOfficial>
2022-03-15 23:08:07 -0300

    update

--- src/core/domain/profile.ts
+++ src/core/domain/profile.ts
@@ -4,37 +4,54 @@ import { IExperience } from './interfaces/experience.interface';
 import { IProfile } from './interfaces/profile.interface';
 
 class Profile implements IProfile {
-  created_at_date: Date;
-  profile_pic_url: string;
-  full_name: string;
-  occupation: string;
-  headline: string;
-  summary: string;
-  experiences: Array<IExperience>;
-  educations: Array<IEducation>;
-  certifications: Array<ICertification>;
-
   constructor(
-    created_at_date: Date,
-    profile_pic_url: string,
-    full_name: string,
-    occupation: string,
-    headline: string,
-    summary: string,
-    experiences: Array<IExperience>,
-    educations: Array<IEducation>,
-    certifications: Array<ICertification>,
-  ) {
-    this.profile_pic_url = profile_pic_url;
-    this.created_at_date = created_at_date;
-    this.full_name = full_name;
-    this.occupation = occupation;
-    this.headline = headline;
-    this.summary = summary;
-    this.experiences = experiences;
-    this.educations = educations;
-    this.certifications = certifications;
-  }
+    private _created_at_date: Date,
+    private _profile_pic_url: string,
+    private _full_name: string,
+    private _occupation: string,
+    private _headline: string,
+    private _summary: string,
+    private _experiences: Array<IExperience>,
+    private _educations: Array<IEducation>,
+    private _certifications: Array<ICertification>,
+  ){}
+
+  public get created_at_date(): Date {
+    return this._created_at_date
+  };
+
+  public get profile_pic_url(): string {
+    return this._profile_pic_url
+  };
+
+  public get full_name(): string {
+    return this._full_name
+  };
+
+  public get occupation(): string {
+    return this._occupation
+  };
+
+  public get headline(): string {
+    return this._headline
+  };
+
+  public get summary(): string {
+    return this._summary
+  };
+
+  public get experiences(): Array<IExperience> {
+    return this._experiences
+  };
+
+  public get educations(): Array<IEducation> {
+    return this._educations
+  };
+
+  public get certifications(): Array<ICertification> {
+    return this._certifications
+  };
+
 }
 
 export { Profile };
